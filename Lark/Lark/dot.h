9de689d050396585267f8f4d07c000cae92cd2ea
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-10-18 16:14:58 +0000

    I created function for open/create graphviz's file.

--- Lark/Lark/dot.h
+++ Lark/Lark/dot.h
@@ -8,3 +8,15 @@ struct VB_Expr
 	struct VB_Expr* FirstSon;   // ”казатель на левого сына.
 	struct VB_Expr* lastSon;    // ”казатель на правого сына.
 };
+
+int open_dot_file ()
+{
+	FILE* file = NULL;
+	int error = fopen_s(&file, "vb_lark.txt", "wt");
+	if (error) return 1;
+
+	error = fprintf(file,"digraph G\n{\n\tgraph [ rankdir = \"LR\" ];\n\tnode [ fontsize = \"16\" shape = \"ellipse\"  ];\n\tedge [ ];");
+	if (error == -1) return 1;
+
+	return 0;
+}
