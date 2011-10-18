6ea6f9e8886d2168c60af365ef01dd8bd6c6654f
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-10-18 16:23:43 +0000

    I fixed small fails for function which open graphViz file. I created function for close graphViz file.

--- trunk/Lark/Lark/dot.h
+++ trunk/Lark/Lark/dot.h
@@ -9,6 +9,10 @@ struct VB_Expr
 	struct VB_Expr* lastSon;    // Указатель на правого сына.
 };
 
+/*!
+    Функция открывает файл для GrphViz и записывает в него верхнюю шапку.
+  \return Если произошла ошибка работы с файлом, то возвращается 1, иначе 0.
+*/
 int open_dot_file ()
 {
 	FILE* file = NULL;
@@ -17,6 +21,26 @@ int open_dot_file ()
 
 	error = fprintf(file,"digraph G\n{\n\tgraph [ rankdir = \"LR\" ];\n\tnode [ fontsize = \"16\" shape = \"ellipse\"  ];\n\tedge [ ];");
 	if (error == -1) return 1;
+
+	fclose(file);
+
+	return 0;
+}
+
+/*!
+    Функция закрывает файл для GrphViz и записывает в него необходимые данные в конец файла.
+  \return Если произошла ошибка работы с файлом, то возвращается 1, иначе 0.
+*/
+int close_dot_file ()
+{
+	FILE* file = NULL;
+	int error = fopen_s(&file, "vb_lark.txt", "at");
+	if (error) return 1;
+
+	error = fprintf(file, "\n};");
+	if (error == -1) return 1;
+
+	fclose(file);
 
 	return 0;
 }
