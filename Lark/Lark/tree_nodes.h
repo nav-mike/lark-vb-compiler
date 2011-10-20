f79b591ed94cf140032489ff57a673ab8baa690c
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-10-20 06:41:21 +0000

    Structures for the Dim operator and for storage identifiers has been created.

--- Lark/Lark/tree_nodes.h
+++ Lark/Lark/tree_nodes.h
@@ -43,7 +43,8 @@ enum VB_Expr_type
 enum VB_Stmt_type
 {
 	EXPR,						//!< Выражение
-	IF_STMT						//!< Операция условного перехода
+	IF_STMT,					//!< Операция условного перехода
+	DIM_STMT					//!< Операция определения переменной
 };
 
 
@@ -56,6 +57,15 @@ enum VB_End_if_stmt_type
 	ELSE,						//!< Есть альтернативное действие
 	ELSE_IF_THEN,				//!< Есть альтернативное действие с условием и на конце условия стоит "Then"
 	ELSE_IF_ENDL				//!< Есть альтернативное действие с условием и на конце условия стоит ENDL
+};
+
+
+enum VB_Id_type
+{
+	INTEGER,
+	BOOLEAN,
+	CHAR,
+	STRING
 };
 
 
@@ -83,10 +93,10 @@ struct VB_Stmt_list
  */
 struct VB_Stmt
 {
-	enum VB_Stmt_type type;			//!< Тип операции.
-	struct VB_Expr* expr;			//!< Указатель на содержащееся в операции выражение.
-	struct VB_Stmt_list* stmt_list;	//!< Указатель на список операций включающихся в данную операцию.
-	struct VB_Stmt* next;			//!< Указатель на следующий элемент последовательности операторов.
+	enum VB_Stmt_type		type;			//!< Тип операции.
+	struct VB_Expr*			expr;			//!< Указатель на содержащееся в операции выражение.
+	struct VB_Stmt_list*	stmt_list;		//!< Указатель на список операций включающихся в данную операцию.
+	struct VB_Stmt*			next;			//!< Указатель на следующий элемент последовательности операторов.
 };
 
 
@@ -95,11 +105,11 @@ struct VB_Stmt
  */
 struct VB_Expr
 {
-	enum VB_Expr_type type;			//!< Тип выражения.
-	char* expr_string;				//!< Имя идентификатора, либо строка.
-	int int_val;					//!< Значение выражения. Используется для bool, int, char.
-	struct VB_Expr* left_chld;		//!< Указатель на левого сына.
-	struct VB_Expr* right_chld;		//!< Указатель на правого сына.
+	enum VB_Expr_type	type;			//!< Тип выражения.
+	char*				expr_string;	//!< Имя идентификатора, либо строка.
+	int					int_val;		//!< Значение выражения. Используется для bool, int, char.
+	struct VB_Expr*		left_chld;		//!< Указатель на левого сына.
+	struct VB_Expr*		right_chld;		//!< Указатель на правого сына.
 };
 
 
@@ -108,11 +118,11 @@ struct VB_Expr
  */
 struct VB_If_stmt
 {
-	bool hasThen;					//!< Есть ли "Then" или после условия идет конец строки
-	struct VB_Expr* expr;			//!< Условие
-	struct VB_Stmt_list* stmt_list;	//!< Указатель на список операций при истинном условии.
-	struct VB_End_if_stmt* end_stmt;//!< Указатель на список операций при ложном условии условии.
-	struct VB_Stmt* next;			//!< Указатель на следующий элемент последовательности операторов.
+	bool					hasThen;	//!< Есть ли "Then" или после условия идет конец строки
+	struct VB_Expr*			expr;		//!< Условие
+	struct VB_Stmt_list*	stmt_list;	//!< Указатель на список операций при истинном условии.
+	struct VB_End_if_stmt*	end_stmt;	//!< Указатель на список операций при ложном условии условии.
+	struct VB_Stmt*			next;		//!< Указатель на следующий элемент последовательности операторов.
 
 };
 
@@ -122,14 +132,62 @@ struct VB_If_stmt
  */
 struct VB_End_if_stmt
 {
-	enum VB_End_if_stmt_type type;		//!< Тип окончания условия
-	struct VB_Expr* expr;				//!< Дополнительное условие, если есть
-	struct VB_Stmt_list* stmt_list;		//!< Указатель на список операций при истинном дополнительном условии.
-	struct VB_End_if_stmt* end_stmt;	//!< Указатель на список операций при ложном дополнительном условии условии.
+	enum VB_End_if_stmt_type	type;		//!< Тип окончания условия
+	struct VB_Expr*				expr;		//!< Дополнительное условие, если есть
+	struct VB_Stmt_list*		stmt_list;	//!< Указатель на список операций при истинном дополнительном условии.
+	struct VB_End_if_stmt*		end_stmt;	//!< Указатель на список операций при ложном дополнительном условии условии.
 };
 
 
+/*! \struct VB_Dim_stmt
+    Структура, описывающая операцию объявления переменных.
+ */
+struct VB_Dim_stmt
+{
+	struct VB_As_Stmt_list* first;		//!< Указатель на первый элемент списка объявляемых переменных
+	struct VB_As_Stmt_list* last;		//!< Указатель на последний элемент списка объявляемых переменных
+};
 
 
+/*! \struct VB_As_Stmt_list
+    Структура, описывающая связный список переменных, объявляемых одним типом
+ */
+struct VB_As_Stmt_list
+{
+	struct VB_As_stmt*		as_stmt;	//!< Список переменных, определяемых одним типом
+	struct VB_As_Stmt_list* next;		//!< Указатель на следующий список переменных определяемых одним типом
+};
 
+
+/*! \struct VB_As_stmt
+    Структура дерева для хранения определения переменных одного типа.
+ */
+struct VB_As_stmt
+{ 
+	bool					isInit;		//!< Инициализируется ли переменная
+	struct VB_Id_list*		idents;		//!< Указатель на идентификаторов
+	struct VB_Expr*			expr;		//!< Выражение, которым переменная инициализируетяс
+};
+
+
+/*! \struct VB_Id_list
+    Структура дерева для хранения списка идентификаторов.
+ */
+struct VB_Id_list
+{
+	struct VB_Id*		current_id;		//!< Текущий идентификатор
+	struct VB_Id_list*	next;			//!< Следующий идентификатор
+};
+
+
+/*! \struct VB_Id
+    Структура дерева для хранения идентификатора.
+ */
+struct VB_Id
+{
+	enum VB_Id_type	type;			//!< Тип, определяемой переменной
+	char*			name;			//!< Имя идентификатора.
+	char*			str_val;		//!< Строковое значение переменной
+	int				int_val;		//!< Значение выражения. Используется для bool, int, char.
+};
 
