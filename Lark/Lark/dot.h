9054c8ae25734b13621ff916345c6a190eb6ba2c
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-11-20 08:37:37 +0000

    Function which add VB_Catch_stmt to dot file, has been added.

--- Lark/Lark/dot.h
+++ Lark/Lark/dot.h
@@ -419,3 +419,31 @@ int add_node_throw_stmt (VB_Throw_stmt* node, int number)
 	fclose(file);
 	return 0;
 }
+
+/*!
+    Функция добавляет в файл GraphViz фрагмент дерева для VB_Catch_stmt.
+  \param module   - узел дерева.
+  \param number   - порядковый номер узла.
+  \return         Если произошла ошибка работы с файлом, то возвращается 1, иначе 0.
+*/
+int add_node_catch_stmt (VB_Catch_stmt* node, int number)
+{
+	FILE* file = NULL;
+	int error = fopen_s(&file,"vb_lark.txt", "at");
+	if (error) return 1;
+
+	error = fprintf(file,"\n\t\"node%d\" [", number);
+	if (error == -1) return 1;
+
+	error = fprintf(file,"\n\t\tlabel = \"<f0> %s | <f1> %s\"",
+		node->id, "CATCH_STMT");
+	if (error == -1) return 1;
+
+	error = fprintf(file,"\n\t\tshape = \"record\"\n\t];");
+	if (error == -1) return 1;
+
+	// Запись stmt
+
+	fclose(file);
+	return 0;
+}
