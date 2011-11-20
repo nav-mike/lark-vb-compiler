7c78b163f68fb753db6ce1ede35cd285255131b5
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-11-20 09:23:04 +0000

    Function which add VB_Param_list to dot file, has been added.

--- Lark/Lark/dot.h
+++ Lark/Lark/dot.h
@@ -559,3 +559,31 @@ int add_node_param_stmt (VB_Param_stmt* node, int number)
 	fclose(file);
 	return 0;
 }
+
+/*!
+    Функция добавляет в файл GraphViz фрагмент дерева для VB_Param_list.
+  \param module   - узел дерева.
+  \param number   - порядковый номер узла.
+  \return         Если произошла ошибка работы с файлом, то возвращается 1, иначе 0.
+*/
+int add_node_param_list (VB_Param_list* node, int number)
+{
+	FILE* file = NULL;
+	int error = fopen_s(&file,"vb_lark.txt", "at");
+	if (error) return 1;
+
+	error = fprintf(file,"\n\t\"node%d\" [", number);
+	if (error == -1) return 1;
+
+	error = fprintf(file,"\n\t\tlabel = \"<f0> %s | <f1> %s \"",
+		"PARAM_LIST", "");
+	if (error == -1) return 1;
+
+	error = fprintf(file,"\n\t\tshape = \"record\"\n\t];");
+	if (error == -1) return 1;
+
+	// Ввод параметров.
+
+	fclose(file);
+	return 0;
+}
