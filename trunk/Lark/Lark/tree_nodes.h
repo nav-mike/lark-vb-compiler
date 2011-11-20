bc21d1a035f4178c8ea055e51676250403dd11df
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2011-11-20 13:14:15 +0000

    Function which create node: VB_Stmt when input data is only one VB_Dim_stmt, has been added.

--- trunk/Lark/Lark/tree_nodes.h
+++ trunk/Lark/Lark/tree_nodes.h
@@ -571,3 +571,38 @@ struct VB_Stmt* create_VB_Stmt_If (struct VB_If_stmt* if_stmt)
 
 	return stmt;
 }
+
+/*!
+	Функция создания операции из инициализации переменной.
+  \param dim_stmt Инициализация перменной.
+  \return Новая операция.
+*/
+struct VB_Stmt* create_VB_Stmt_Dim (struct VB_Dim_stmt* dim_stmt)
+{
+	struct VB_Stmt* stmt = NULL;
+
+	if (dim_stmt != NULL)
+	{
+		stmt = (struct VB_Stmt*)malloc(sizeof(struct VB_Stmt));
+		stmt->expr = NULL;
+		stmt->dim_stmt = dim_stmt;
+		stmt->do_loop_stmt = NULL;
+		stmt->enum_stmt = NULL;
+		stmt->for_stmt = NULL;
+		stmt->func_stmt = NULL;
+		stmt->if_stmt = NULL;
+		stmt->next = NULL;
+		stmt->print_stmt = NULL;
+		stmt->println_stmt = NULL;
+		stmt->read_stmt = NULL;
+		stmt->readkey_stmt = NULL;
+		stmt->readln_stmt = NULL;
+		stmt->sub_stmt = NULL;
+		stmt->throw_stmt = NULL;
+		stmt->try_catch_stmt = NULL;
+		stmt->type = DIM;
+		stmt->while_stmt = NULL;
+	}
+
+	return stmt;
+}
