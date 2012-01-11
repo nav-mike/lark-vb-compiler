31557347a895c5782654934a293b1a32532fec2b
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-11 09:08:04 +0000

    This is a subproject. Main reason for create this project is develop and test function for create graphViz's file. In this project new functions have more better structures and inputs parameters.

---
+++ Lark/Dot/main.c
@@ -0,0 +1,99 @@
+﻿#include <stdio.h>
+
+#include"tree_nodes.h"
+
+int Number;
+
+/*!
+	\brief Функция открытия файла для GraphViz.
+	\param file Дескриптор открытого файла.
+	\param filename Имя открываемого файла.
+	\return 0 если ошибок нет.
+*/
+int open_gv_file (FILE* file, char* filename)
+{
+	int result = 0;
+
+	result = fopen_s(&file,filename,"wt");
+
+	if (result)
+		return 1;
+
+	result = fprintf(file,
+		"digraph G\n{\n\tgraph [ rankdir = \"LR\" ];\n\tnode [ fontsize = \"16\" shape = \"ellipse\"  ];\n\tedge [ ];");
+	if (result == -1)
+		return 1;
+
+	return 0;
+}
+
+int add_declaration_list (FILE* file, struct VB_Decl_stmt_list* list)
+{
+	return 0;
+}
+
+int add_statement_list (FILE* file, struct VB_Stmt_list* list)
+{
+	return 0;
+}
+
+/*!
+	\brief Функция добавления модуля в файл.
+	\param file Дескриптор файла.
+	\param module Добавляемый модуль.
+	\return 0 если ошибок нет.
+*/
+int add_module_statement (FILE* file, struct VB_Module_stmt* module)
+{
+	int error = fprintf(file,"\n\nt\"\"node%d\" [\n\t\tlabel = \"<f0> Module | <f1> %s \"\n\t\tshape = \"record\"\n\t];",
+		Number, module->id);
+	if (error == -1)
+		return 1;
+
+	if (module->decl_list != NULL)
+	{
+		error = fprintf(file,"\n\t\"node%d\":f0 -> \"node%d\":f0;",
+			Number, Number + 1);
+		if (error == -1)
+			return 1;
+		Number++;
+		error = add_declaration_list(file, module->decl_list);
+		if (error)
+			return 1;
+	}
+
+	if (module->stmt_list != NULL)
+	{
+		error = fprintf(file,"\n\t\"node%d\":f0 -> \"node%d\":f0;",
+			Number, Number + 1);
+		if (error == -1)
+			return 1;
+		Number++;
+		error = add_statement_list(file, module->stmt_list);
+		if (error)
+			return 1;
+	}
+
+	return 0;
+}
+
+/*!
+	\brief Функция закрытия файла для GraphViz.
+	\param file Дескриптор закрываемого файла.
+	\return 0 если ошибок нет.
+*/
+int close_gv_file (FILE* file)
+{
+	int error = fprintf(file, "\n};");
+	if (error == -1)
+		return 1;
+	
+	fclose(file);
+
+	return 0;
+}
+
+int main (int argc, char* argv[])
+{
+	return 0;
+}
