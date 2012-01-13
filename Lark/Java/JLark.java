ec1a401e94094b6127a9a1a451c8c0a04496d3a5
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-13 18:39:37 +0000

    I created code which run exe file from Java code.

--- Lark/Java/JLark.java
+++ Lark/Java/JLark.java
@@ -4,6 +4,10 @@
  */
 package jlark;
 
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mikhail Navrotskiy
@@ -14,6 +18,50 @@ public class JLark {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            String line;
+        OutputStream stdin = null;
+        InputStream stderr = null;
+        InputStream stdout = null;
+
+          // launch EXE and grab stdin/stdout and stderr
+          Process process = Runtime.getRuntime ().exec ("__lark__.exe test.txt");
+//          stdin = process.getOutputStream ();
+          stderr = process.getErrorStream ();
+          stdout = process.getInputStream ();
+
+//          // "write" the parms into stdin
+//          line = "param1" + "\n";
+//          stdin.write(line.getBytes() );
+//          stdin.flush();
+//
+//          line = "param2" + "\n";
+//          stdin.write(line.getBytes() );
+//          stdin.flush();
+//
+//          line = "param3" + "\n";
+//          stdin.write(line.getBytes() );
+//          stdin.flush();
+//
+//          stdin.close();
+//
+          // clean up if any output in stdout
+          BufferedReader brCleanUp =
+            new BufferedReader (new InputStreamReader (stdout));
+          while ((line = brCleanUp.readLine ()) != null) {
+            System.out.println ("[Stdout] " + line);
+          }
+          brCleanUp.close();
+
+          // clean up if any output in stderr
+          brCleanUp =
+            new BufferedReader (new InputStreamReader (stderr));
+          while ((line = brCleanUp.readLine ()) != null) {
+            System.out.println ("[Stderr] " + line);
+          }
+          brCleanUp.close();
+        } catch (IOException ex) {
+            Logger.getLogger(JLark.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
