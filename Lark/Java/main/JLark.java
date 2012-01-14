1ec24854b399a05db74956ae3351be438dedbefc
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 19:13:45 +0000

    Table of constants can be demonstrate.

--- Lark/Java/main/JLark.java
+++ Lark/Java/main/JLark.java
@@ -4,9 +4,12 @@
  */
 package main;
 
+import guidebug.IntFrame;
+import guidebug.TableConstant;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JTable;
 
 /**
  *
@@ -18,50 +21,58 @@ public class JLark {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            String line;
-        OutputStream stdin = null;
-        InputStream stderr = null;
-        InputStream stdout = null;
-
-          // launch EXE and grab stdin/stdout and stderr
-          Process process = Runtime.getRuntime ().exec ("__lark__.exe test.txt");
-//          stdin = process.getOutputStream ();
-          stderr = process.getErrorStream ();
-          stdout = process.getInputStream ();
-
-//          // "write" the parms into stdin
-//          line = "param1" + "\n";
-//          stdin.write(line.getBytes() );
-//          stdin.flush();
-//
-//          line = "param2" + "\n";
-//          stdin.write(line.getBytes() );
-//          stdin.flush();
-//
-//          line = "param3" + "\n";
-//          stdin.write(line.getBytes() );
-//          stdin.flush();
-//
-//          stdin.close();
-//
-          // clean up if any output in stdout
-          BufferedReader brCleanUp =
-            new BufferedReader (new InputStreamReader (stdout));
-          while ((line = brCleanUp.readLine ()) != null) {
-            System.out.println ("[Stdout] " + line);
-          }
-          brCleanUp.close();
-
-          // clean up if any output in stderr
-          brCleanUp =
-            new BufferedReader (new InputStreamReader (stderr));
-          while ((line = brCleanUp.readLine ()) != null) {
-            System.out.println ("[Stderr] " + line);
-          }
-          brCleanUp.close();
-        } catch (IOException ex) {
-            Logger.getLogger(JLark.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
+        TableConstant tc = new TableConstant();
+        JTable[] tables = new JTable[1];
+        String[] titles = new String[1];
+        tc.addRow("UTF-8", "mass");
+        tables[0] = tc.getTable();
+        titles[0] = "Таблица констант";
+        IntFrame intFrame = new IntFrame(tables, titles);
+        //        try {
+        //            String line;
+        //        OutputStream stdin = null;
+        //        InputStream stderr = null;
+        //        InputStream stdout = null;
+        //
+        //          // launch EXE and grab stdin/stdout and stderr
+        //          Process process = Runtime.getRuntime ().exec ("__lark__.exe test.txt");
+        ////          stdin = process.getOutputStream ();
+        //          stderr = process.getErrorStream ();
+        //          stdout = process.getInputStream ();
+        //
+        ////          // "write" the parms into stdin
+        ////          line = "param1" + "\n";
+        ////          stdin.write(line.getBytes() );
+        ////          stdin.flush();
+        ////
+        ////          line = "param2" + "\n";
+        ////          stdin.write(line.getBytes() );
+        ////          stdin.flush();
+        ////
+        ////          line = "param3" + "\n";
+        ////          stdin.write(line.getBytes() );
+        ////          stdin.flush();
+        ////
+        ////          stdin.close();
+        ////
+        //          // clean up if any output in stdout
+        //          BufferedReader brCleanUp =
+        //            new BufferedReader (new InputStreamReader (stdout));
+        //          while ((line = brCleanUp.readLine ()) != null) {
+        //            System.out.println ("[Stdout] " + line);
+        //          }
+        //          brCleanUp.close();
+        //
+        //          // clean up if any output in stderr
+        //          brCleanUp =
+        //            new BufferedReader (new InputStreamReader (stderr));
+        //          while ((line = brCleanUp.readLine ()) != null) {
+        //            System.out.println ("[Stderr] " + line);
+        //          }
+        //          brCleanUp.close();
+        //        } catch (IOException ex) {
+        //            Logger.getLogger(JLark.class.getName()).log(Level.SEVERE, null, ex);
+        //        }
     }
 }
