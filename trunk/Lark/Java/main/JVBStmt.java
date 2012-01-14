0466d911de8d8028b89ab70905641a37e049c486
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 16:57:51 +0000

    All constructors for JVBStmt class have been created and added.

--- trunk/Lark/Java/main/JVBStmt.java
+++ trunk/Lark/Java/main/JVBStmt.java
@@ -1,6 +1,8 @@
 package main;
 
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * Класс, описывающий операцию.
@@ -16,8 +18,62 @@ public class JVBStmt {
     /** Ссылка на следующую операцию. */
     private JVBStmt m_next;
 
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует все null.
+     */
+    public JVBStmt() {
+        
+        m_next = null;
+        m_type = null;
+        m_value = null;
+    }
+
+    /**
+     * Конструктор с параметрами.
+     * Инициализирует объект входными параметрами.
+     * @param m_type Тип операции.
+     * @param m_value Ссылка на операцию.
+     * @param m_next Следующая операция.
+     */
+    public JVBStmt(JVBStmtType m_type, Object m_value, JVBStmt m_next) {
+        this.m_type = m_type;
+        this.m_value = m_value;
+        this.m_next = m_next;
+    }
+
+    /**
+     * Конструктор с параметрами.
+     * Инициализирует объект узлом XML дерева.
+     * @param item Узел XML дерева.
+     */
     JVBStmt(Node item) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        
+        this();
+        String buffer;
+        NamedNodeMap attributes = item.getAttributes();
+        // Считывание типа операции.
+        Node attr = attributes.getNamedItem("type");
+        buffer = attr.getNodeValue();
+        m_type = JVBStmtType.valueOf(buffer);
+        // Считывание вложенных структур.
+        NodeList nodes = item.getChildNodes();
+        if (m_type == JVBStmtType.DIM_E)
+            m_value = new JVBDimStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.DO_LOOP_E)
+            m_value = new JVBDoLoopStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.FOR_E)
+            m_value = new JVBForStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.FUNC_D)
+            m_value = new JVBFucnStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.IF_E)
+            m_value =  new JVBIfStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.RETURN_E)
+            m_value = new JVBReturnStmt(nodes.item(0));
+        else if (m_type == JVBStmtType.STMT_EXPR_E)
+            m_value = new JVBExpr(nodes.item(0));
+        else if (m_type == JVBStmtType.WHILE_E)
+            m_value = new JVBWhileStmt(nodes.item(0));
     }
     
     /**
