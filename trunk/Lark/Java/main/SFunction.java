019b0abf9b4b02b78cd45be2c43cc8a3c8e725b1
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 21:12:12 +0000

    Class SFunction which this is a one of function from table of function of some module, has been added.

---
+++ trunk/Lark/Java/main/SFunction.java
@@ -0,0 +1,91 @@
+package main;
+
+import java.util.HashMap;
+
+/**
+ * Элемент таблицы методов.
+ * @version 1.0
+ */
+public class SFunction {
+    
+    /* Поля класса. */
+    /** Ссылка на таблицу констант. */
+    private SConstant methodRef;
+    /** Локальные переменные. */
+    private HashMap<String, SLocalVariable> locals;
+    /** Ссылка на функцию (Процедуру). */
+    private Object func;
+
+    /**
+     * Метод задания ссылки на таблицу констант.
+     * @param methodRef Таблица констант.
+     */
+    public void setMethodRef(SConstant methodRef) {
+        this.methodRef = methodRef;
+    }
+
+    /**
+     * Метод задания списка локальных переменных.
+     * @param locals Локальные переменные.
+     */
+    public void setLocals(HashMap<String, SLocalVariable> locals) {
+        this.locals = locals;
+    }
+
+    /**
+     * Метод задания ссылки на функцию.
+     * @param func Ссылка на функцию.
+     */
+    public void setFunc(Object func) {
+        this.func = func;
+    }
+
+    /**
+     * Метод полчения ссылки на таблицу констант.
+     * @return Ссылка на таблицу констант.
+     */
+    public SConstant getMethodRef() {
+        return methodRef;
+    }
+
+    /**
+     * Метод получения локальных переменных.
+     * @return Локальные переменные.
+     */
+    public HashMap<String, SLocalVariable> getLocals() {
+        return locals;
+    }
+
+    /**
+     * Метод получения ссылки на функцию.
+     * @return Ссылка на функцию.
+     */
+    public Object getFunc() {
+        return func;
+    }
+
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует null.
+     */
+    public SFunction() {
+        
+        func = null;
+        locals = new HashMap<String, SLocalVariable>();
+        methodRef = null;
+    }
+
+    /**
+     * Конструктор с параметрами.
+     * Инициализирует объект входынми параметрами.
+     * @param methodRef Ссылка на таблицу констант.
+     * @param locals Локальные переменные.
+     * @param func Функция (Процедура).
+     */
+    public SFunction(SConstant methodRef, HashMap<String, SLocalVariable> locals, Object func) {
+        this.methodRef = methodRef;
+        this.locals = locals;
+        this.func = func;
+    }
+    
+}
