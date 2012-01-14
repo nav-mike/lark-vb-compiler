63a07592b1631e33133c27f29ba6b04198c19ec8
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 18:24:32 +0000

    In class which keep and show table of constants have been added all members.

--- Lark/Java/guidebug/TableConstant.java
+++ Lark/Java/guidebug/TableConstant.java
@@ -1,9 +1,22 @@
 package guidebug;
 
+import java.util.HashMap;
+import javax.swing.JTable;
+
 /**
  * Класс таблицы констант.
  * @author Mikhail Navrotskiy
  */
 public class TableConstant {
+    
+    /* Поля класса. */
+    /** Первая часть виртуального отображения таблицы (Номер константы и тип). */
+    private HashMap<Integer,String> part1;
+    /** Вторая часть виртуального отображения таблицы (Номер константы и значение). */
+    private HashMap<Integer,String> part2;
+    /** Поле для отладки: графическое отображение таблицы. */
+    private JTable table;
+    /** Столбцы класса. */
+    private static final String[] colNames = {"ConstNum","Type","Value"};
     
 }
