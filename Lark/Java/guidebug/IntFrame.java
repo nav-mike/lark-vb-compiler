e4f983fcc9b1b7c73a189d6496aa6706dbbbf20b
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 18:58:01 +0000

    Class for show internal frame which demonstrate table of constants has been added (with constructor).

---
+++ Lark/Java/guidebug/IntFrame.java
@@ -0,0 +1,35 @@
+package guidebug;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+/**
+ * Класс, отображающий все нужные таблицы.
+ * @author Mikhail Navrotskiy
+ */
+public class IntFrame extends JFrame {
+
+    /**
+     * Конструктор с параметрами.
+     * Создает окно с таблицами.
+     * @param tables Массив требуемых таблиц.
+     * @param titles Масси заголовков окон.
+     */
+    public IntFrame (JTable[] tables, String[] titles){
+        
+        super("Лабораторная работа №4");
+        
+        for (int i = 0; i < tables.length; i++) {
+            
+            MyFrame fr = new MyFrame(tables[i], titles[i]);
+            add(fr.getFrame());
+        }
+        
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    
+    
+}
