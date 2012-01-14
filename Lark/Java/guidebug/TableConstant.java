1c777d1b57f87aa89e074877750981400234cd40
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-14 18:44:25 +0000

    To class which keep and show table of constants I added: default constructor, getter for table and method which add new row.

--- Lark/Java/guidebug/TableConstant.java
+++ Lark/Java/guidebug/TableConstant.java
@@ -16,7 +16,44 @@ public class TableConstant {
     private HashMap<Integer,String> part2;
     /** Поле для отладки: графическое отображение таблицы. */
     private JTable table;
+    /** Размерность таблицы. */
+    private int size;
     /** Столбцы класса. */
     private static final String[] colNames = {"ConstNum","Type","Value"};
+
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует пустую таблицу.
+     */
+    public TableConstant() {
+        
+        part1 = new HashMap<Integer, String>();
+        part2 = new HashMap<Integer, String>();
+        table = new JTable(null, colNames);
+        size = 0;
+    }
+
+    /**
+     * Метод полчения ссылки на таблицу.
+     * @return Ссылка на таблицу.
+     */
+    public JTable getTable() {
+        return table;
+    }
+    
+    /**
+     * Метод добавления строчки в таблицу констант.
+     * @param type Тип строки.
+     * @param value Значение строки.
+     */
+    public void addRow (String type, String value) {
+        
+        part1.put(size, type);
+        part2.put(size, value);
+        table.setValueAt(size, size, 0);
+        table.setValueAt(type, size, 1);
+        table.setValueAt(value, size, 2);
+        size++;
+    }
     
 }
