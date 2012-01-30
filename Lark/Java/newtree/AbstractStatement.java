abe2c4bc98700c2440e062248acc36018381f0ff
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-30 17:58:32 +0000

    New class of abstract statement has been added.

---
+++ Lark/Java/newtree/AbstractStatement.java
@@ -0,0 +1,36 @@
+package newtree;
+
+/**
+ * Класс абстрактного оператора.
+ * Суперкласс для всех типов операторов.
+ * @version 1.0
+ */
+public class AbstractStatement {
+    
+    /* Поля класса. */
+    /** Тип оператора. */
+    protected int stmtType;
+    /** Оператор - выражение. */
+    public static final int EXPRESSION = 11;
+    /** Оператор - условие. */
+    public static final int IF         = 12;
+    /** Оператор - объявление переменной. */
+    public static final int DIM        = 13;
+    /** Оператор - цикл for. */
+    public static final int FOR        = 14;
+    /** Оператор - цикл while. */
+    public static final int WHILE      = 15;
+    /** Оператор - цикл do...loop. */
+    public static final int DO_LOOP    = 16;
+    /** Оператор - возврат из функции. */
+    public static final int RETURN     = 17;
+    /** Оператор - чтение символа из стандартного потока ввода. */
+    public static final int READ       = 18;
+    /** Оператор - чтение строки из стандартного потока ввода. */
+    public static final int READLINE   = 19;
+    /** Оператор - запись в стандартный поток вывода. */
+    public static final int PRINT      = 10;
+    /** Оператор - запись в стандартный поток вывода с переводом строки. */
+    public static final int PRINTLINE  = 20;
+    
+}
