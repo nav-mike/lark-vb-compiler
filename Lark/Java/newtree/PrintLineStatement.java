b4323bf20f856212b0b29a5a7504649cdb82a776
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-01-31 05:42:15 +0000

    New class of write expression with newline symbol statement has been added.

---
+++ Lark/Java/newtree/PrintLineStatement.java
@@ -0,0 +1,42 @@
+package newtree;
+
+/**
+ * Класс оператора вывода выражения в стандартный поток вывода,
+ * с символом переноса строки.
+ * @version 1.0
+ */
+public class PrintLineStatement extends AbstractStatement {
+    
+    /* Поля класса. */
+    /** Выражение, выводимое в стандартный поток вывода. */
+    private Expression printedData;
+
+    /**
+     * Метод получения выражения, выводимого в стандартный поток вывода.
+     * @return Выржаение, выводимое в стандартный поток вывода.
+     */
+    public Expression getPrintedData() {
+        return printedData;
+    }
+
+    /**
+     * Метод задания выражения, выводимого в стандартный поток вывода.
+     * @param printedData Выражение, выводимое в стандартный поток вывода.
+     */
+    public void setPrintedData(Expression printedData) {
+        this.printedData = printedData;
+    }
+
+    /**
+     * Конструктор с параметром.
+     * Создает оператор с заданным выражением, выводимым в стандартный поток 
+     * вывода.
+     * @param printedData Выражение, выводимое в стандартный поток вывода.
+     */
+    public PrintLineStatement(Expression printedData) {
+        
+        super(PRINTLINE);
+        this.printedData = printedData;
+    }
+    
+}
