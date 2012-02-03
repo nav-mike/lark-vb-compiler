a8afbe867fa4ea08b540caede32466a370220319
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-03 21:25:12 +0000

    Function of reading parameters of procedures or functions has been realized.

--- Lark/Java/newtree/ParamStatement.java
+++ Lark/Java/newtree/ParamStatement.java
@@ -1,10 +1,14 @@
 package newtree;
 
+import main.JVBIdType;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
 /**
  * Класс, описывающий параметр функции.
  * @version 1.0
  */
-public class ParamStatement {
+public class ParamStatement implements XMLInterface{
     
     /* Поля класса. */
     /** Имя параметра. */
@@ -14,6 +18,16 @@ public class ParamStatement {
     /** Тип параметра. */
     private DataType type;
 
+    /**
+     * Конструктор по умолчанию
+     */
+    public ParamStatement(){
+        this.name = null;
+        this.isArray = false;
+        this.type = DataType.NONE;
+    }
+    
+    
     /**
      * Конструктор с параметрами.
      * Создает параметр функции по заданным входным данным.
@@ -73,6 +87,32 @@ public class ParamStatement {
      */
     public void setType(DataType type) {
         this.type = type;
+    }
+
+    @Override
+    public void readData(Node node) {
+        
+        String buffer;
+        
+        NamedNodeMap attributes = node.getAttributes();
+        // Считывание передачи по ссылке.
+        Node attr = attributes.getNamedItem("is_by_ref");
+        buffer = attr.getNodeValue();
+                  
+        if (buffer.equals("0"))
+            isArray = false;
+        else
+            isArray = true;
+        
+        // Считывание имени параметра.
+        attr = attributes.getNamedItem("id");
+        buffer = attr.getNodeValue();
+        name = buffer;
+        
+        // Считывание типа параметра.
+        attr = attributes.getNamedItem("id_type");
+        buffer = attr.getNodeValue();
+        type = DataType.fromString(buffer);
     }
     
 }
