cad1ce967beea9f64a471cba5b6d66a81275a4b5
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-08 15:53:11 +0000

    Class for local variables table item has been added.

---
+++ trunk/Lark/Java/tables/LocalVariablesTableItem.java
@@ -0,0 +1,95 @@
+package tables;
+
+import newtree.DataType;
+
+/**
+ * Класс локальной переменной.
+ * Хранит в себе информацию о локальных переменных функции.
+ * @version 1.0
+ */
+public class LocalVariablesTableItem {
+    
+    /* Поля класса. */
+    /** Имя локальной переменной. */
+    private String name;
+    /** Тип локальной переменной. */
+    private DataType type;
+    /** Индекс локальной переменной в таблице локальных переменных. */
+    private int number;
+
+    /**
+     * Метод задания типа локальной переменной.
+     * @param type Тип локальной переменной.
+     * @throws InvalidParametersException Исключение вызываемое когда тип переменной
+     * задается как NONE.
+     */
+    public void setType(DataType type) throws InvalidParametersException {
+        
+        if (type == DataType.NONE)
+            throw new InvalidParametersException("Неверный тип локальной переменной!");
+        
+        this.type = type;
+    }
+
+    /**
+     * Метод полчения индекса локальной переменной в таблице локальных
+     * переменных.
+     * @param number Индекс в таблице локальных переменных.
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * Метод задания имени локальной переменной.
+     * @param name Имя локальной переменной.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Метод получения типа локальной переменной.
+     * @return Тип локальной переменной.
+     */
+    public DataType getType() {
+        return type;
+    }
+
+    /**
+     * Метод получения индекса локальной переменной в таблице локальных 
+     * переменных.
+     * @return Индекс в таблице локальных переменных.
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Метод получения имени локальной переменной.
+     * @return Имя локальной переменной.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Конструктор с параметрами.
+     * Создает объект с заданными параметрами.
+     * @param name Имя локальной переменной.
+     * @param type Тип локальной переменной.
+     * @param number Индекс локальной переменной в таблице локальных переменных.
+     * @throws InvalidParametersException Исключение вызываемое когда тип переменной
+     * задается как NONE.
+     */
+    public LocalVariablesTableItem(String name, DataType type, int number) throws InvalidParametersException {
+        
+        if (type == DataType.NONE)
+            throw new InvalidParametersException("Неверный тип локальной переменной!");
+        
+        this.name = name;
+        this.type = type;
+        this.number = number;
+    }
+    
+}
