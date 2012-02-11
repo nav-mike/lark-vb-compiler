f3cdec09cdc2057ac9ab8725970413c318e9570d
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-11 11:16:24 +0000

    Constanta's table for demonstration all constants of user's main module, has been added. And, table is with user's functions/procedures.

--- Lark/Java/finderros/FillTables.java
+++ Lark/Java/finderros/FillTables.java
@@ -1,5 +1,8 @@
 package finderros;
 
+import java.util.ArrayList;
+import newtree.AbstractDeclaration;
+import newtree.DataType;
 import newtree.Module;
 import tables.ConstantsTable;
 import tables.ConstantsTableItem;
@@ -25,13 +28,86 @@ public class FillTables {
         return ct;
     }
     
+    /**
+     * Метод заполнения таблицы констант главного (пользовательского) модуля.
+     * @param item Узел дерева.
+     * @return Заполненная таблица констант.
+     * @throws InvalidParametersException Исключение выбрасывается при неверном
+     * создании констант.
+     */
     public static ConstantsTable fillMainClassConstatntsTable (Module item) throws InvalidParametersException {
         
         ConstantsTable ct = new ConstantsTable();
         
         writeStartConstatntsToTable(ct);
         
+        Integer index = ct.size();
+        
+        ct.add(new ConstantsTableItem(++index, item.getId()));
+        ct.add(ConstantsTableItem.CreateClassConst(++index, ct.get(index-1)));
+        
+        fillMainClassConstantsTableByMethods(ct, item.getDeclList(), ++index, 
+                index - 1);
+        
         return ct;
+    }
+    
+    /**
+     * Метод заполнения таблицы констант методами пользователя пользовательского
+     * модуля.
+     * @param ct Таблица констант.
+     * @param items Список методов.
+     * @param size Размер таблицы констант.
+     * @param classIndex Индекс константы модуля (класса) в таблице констант.
+     * @throws InvalidParametersException Исключение выбрасывается при неверном
+     * создании констант.
+     */
+    private static void fillMainClassConstantsTableByMethods (ConstantsTable ct,
+            ArrayList<AbstractDeclaration> items, Integer size, int classIndex) throws InvalidParametersException {
+        
+        int j = 0;
+        for (int i = 0; i < items.size(); i++) {
+            
+            int index1 = size + j++, index2 = size + j++, index3 = size + j++;
+            ct.add(new ConstantsTableItem(index1, items.get(i).getName()));
+            ct.add(new ConstantsTableItem(index2, 
+                    constructDescriptorOfMethod(items.get(i))));
+            ct.add(new ConstantsTableItem(index3,ct.get(index1), ct.get(index2)));
+            ct.add(ConstantsTableItem.CreateMethodRefConst(size + j++,
+                    ct.get(classIndex), ct.get(index3)));
+            
+        }
+        
+    }
+    
+    /**
+     * Метод получения дескриптора функции.
+     * @param item Функция.
+     * @return Дескриптор функции.
+     */
+    private static String constructDescriptorOfMethod (AbstractDeclaration item) {
+        
+        String result = "(";
+        
+        if (item.getParamList() != null) {
+            
+            for (int i = 0; i < item.getParamList().size(); i++) {
+
+                result += item.getFromParamList(i).getType().convertToConstantsTablesString();
+
+                if (i != item.getParamList().size() - 1)
+                    result +=",";
+            }
+        }
+        
+        result += ")";
+        
+        result += item.getRetType().convertToConstantsTablesString();
+        
+        result += ";";
+        
+        return result;
+        
     }
     
     /**
