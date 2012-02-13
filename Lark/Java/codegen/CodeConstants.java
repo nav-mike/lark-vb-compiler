062090e9d593e4afdbbe86eee5295213ff2e5d46
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-13 19:38:28 +0000

    The package for code generation with generator class and enumeration of constants for code generation has been created.

---
+++ Lark/Java/codegen/CodeConstants.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codegen;
+
+/**
+ * Перечисление констант для генерации кода.
+ * @version 1.0
+ */
+public enum CodeConstants {
+    
+}
---
+++ Lark/Java/codegen/CodeGenerator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codegen;
+
+/**
+ * Класс для генерации кода дерева.
+ * @version 1.0
+ */
+public class CodeGenerator {
+    
+}
