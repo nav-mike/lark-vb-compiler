940031b4a7d01436c223b0e329a52b6d290fd05f
SashkaCosmonaut@gmail.com <SashkaCosmonaut@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-19 13:28:18 +0000

    Numbers of important fields from constant table have been added.

--- Lark/Java/codegen/CodeConstants.java
+++ Lark/Java/codegen/CodeConstants.java
@@ -5,9 +5,56 @@
 package codegen;
 
 /**
- * Перечисление констант для генерации кода.
+ * Номера важных констант из таблицы констант.
  * @version 1.0
  */
-public enum CodeConstants {
+public class CodeConstants {
     
+    /** Номер поля Code. */
+    public static int CODE;
+    
+    /** Номер строки с класом String. */
+    public static int STRING;
+    
+    /** Номер класса Object. */
+    public static int OBJECT_CLASS;
+        
+    /** Номер класса Console. */
+    public static int CONSOLE_CLASS;
+    
+    /** Номер текущего класса. */
+    public static int CURRENT_CLASS;
+    
+    /** Номер конструктора Object. */
+    public static int OBJECT_INIT;
+    
+    /** Номер конструктора текущего класса. */
+    public static int CURRENT_INIT;
+    
+    /** RTL - Записать строку с переносом. */
+    public static int WRITE_LINE_STRING;
+    
+    /** RTL - Записать число с переносом. */
+    public static int WRITE_LINE_INT;
+    
+    /** RTL - Записать булеву переменную с переносом. */
+    public static int WRITE_LINE_BOOLEAN;
+    
+    /** RTL - Записать строку без переноса. */
+    public static int WRITE_STRING;
+    
+    /** RTL - Записать число без переноса. */
+    public static int WRITE_INT;
+    
+    /** RTL - Записать булеву переменную без переноса. */
+    public static int WRITE_BOOLEAN;
+    
+    /** RTL - Считать строку. */
+    public static int READ_LINE_STRING;
+    
+    /** RTL - Считать число. */
+    public static int STRING_TO_INT;
+    
+    /** RTL - Считать булевую переменную. */
+    public static int STRING_TO_BOOLEAN;
 }
