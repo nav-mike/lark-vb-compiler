46c17921930322c8b4f038cae7410194662131d3
m.navrotskiy@gmail.com <m.navrotskiy@gmail.com@12ac27aa-052f-fd4f-b552-7cd0277d927a>
2012-02-23 13:40:51 +0000

    Qwe Qwe

---
+++ trunk/Lark/Java/codegen/ArrayType.java
@@ -0,0 +1,16 @@
+package codegen;
+
+/**
+ * Класс, описывающий тип массива.
+ * @version 1.0
+ */
+public class ArrayType {
+    
+    /** Массив логических значений. */
+    public static final byte T_BOOLEAN = 0x4;
+    /** Массив символов. */
+    public static final byte T_CHAR    = 0x5;
+    /** Массив целых чисел. */
+    public static final byte T_INT     = 0xA;
+    
+}
